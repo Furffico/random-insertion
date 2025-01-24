162f21ac006a4a95c36de1a3689105bb1cd2ebe9
Furffico <jerrywang0206@gmail.com>
2025-01-24 23:34:02 +0800

    fix: windows build args (release v0.3.0)

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import Extension, setup
 import glob, os
 import numpy
+import sys
+
+if sys.platform == "win32":
+    extra_compile_args = ["/std:c++17", "/O2"]
+else:
+    extra_compile_args = ["-std=c++17", "-O3", "-Os"]
 
 numpy_header_path = os.path.join(
     os.path.dirname(numpy.__file__), 
@@ -12,7 +18,7 @@ _core_ext = Extension(
     name = "random_insertion._core",
     sources = glob.glob("random_insertion/src/*.cpp"),
     include_dirs = ["random_insertion/src", numpy_header_path],
-    extra_compile_args = ["-std=c++17", "-O3", "-Os"],
+    extra_compile_args = extra_compile_args,
     language = "c++",
 )
 
